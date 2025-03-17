48b4971c233e67f8c22699344d976505f83f62ac
Andrew Ahn <andrew.ahn@hotmail.co.uk>
2025-03-17 17:31:11 +0000

    experiment with location of files

---
+++ baseIndex21-30.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="baseStyle.css">
+    <title>50 Projects in 50 Days!</title>
+</head>
+
+<body>
+<?php include 'header.php'; ?>
+
+    <div class="px-4 py-5 my-5 text-center container-fluid">
+        <p class="intro-space">
+            <!-- This intro space is needed to push the below image down properly -->
+        </p>
+        <img class="d-block mx-auto mb-4" src="../Base/baseMedia/inoveryourhead.jpg" alt="" width="500" height="500">
+        <h1 class="display-5 fw-bold text-body-emphasis"> Projects 21 - 30!</h1>
+
+        <div class="container-fluid col-lg-12 mx-auto">
+            <p class="lead mb-4 text-center intro-text">This is a learning course from <a
+                href="http://https://www.udemy.com/course/50-projects-50-days/learn/lecture/23595148#overview">Udemy</a>,
+            wrapped in a functional website using Bootstrap,
+            to allow people to navigate to the individual projects.
+            Challenges faced were in creating a website to wrap/contain all the projects, because
+            the course format is for 50 individual projects, 50 github entries.
+            <br>
+            <br>At several points here, I felt I was drowning!</p>
+
+        </div>
+ 
+
+    <div class="container px-4 py-1 my-1 text-center">
+            
+             <!-- This is the 5th row -->        
+             <div class="row form-group gx-2 gy-2 mt-5">
+                <h5>Projects 21 - 25</h5>
+                <div class="card mx-2" style="width: 15rem;" style="height: 10rem;">
+                    <img src="../Base/baseMedia/ladyOnRocket.gif" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Project  - Lorem, ipsum.</h5>
+                        <p class="card-text"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, quia.</p>
+                        <a href="" class="btn btn-primary"> Lorem, ipsum.
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card mx-2" style="width: 15rem;" style="height: 10rem;">
+                    <img src="../Base/baseMedia/ladyOnRocket.gif" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Project  - Lorem, ipsum.</h5>
+                        <p class="card-text"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, quia.</p>
+                        <a href="" class="btn btn-primary"> Lorem, ipsum.
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card mx-2" style="width: 15rem;" style="height: 10rem;">
+                    <img src="../Base/baseMedia/ladyOnRocket.gif" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Project  - Lorem, ipsum.</h5>
+                        <p class="card-text"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, quia.</p>
+                        <a href="" class="btn btn-primary"> Lorem, ipsum.
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card mx-2" style="width: 15rem;" style="height: 10rem;">
+                    <img src="../Base/baseMedia/ladyOnRocket.gif" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Project  - Lorem, ipsum.</h5>
+                        <p class="card-text"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, quia.</p>
+                        <a href="" class="btn btn-primary"> Lorem, ipsum.
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card mx-2" style="width: 15rem;" style="height: 10rem;">
+                    <img src="../Base/baseMedia/ladyOnRocket.gif" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Project  - Lorem, ipsum.</h5>
+                        <p class="card-text"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, quia.</p>
+                        <a href="" class="btn btn-primary"> Lorem, ipsum.
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+
+             <!-- This is the 6th row -->        
+             <div class="row form-group gx-2 gy-2 mt-5">
+                <h5>Projects 26 - 30</h5>
+                <div class="card mx-2" style="width: 15rem;" style="height: 10rem;">
+                    <img src="../Base/baseMedia/ladyOnRocket.gif" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Project  - Lorem, ipsum.</h5>
+                        <p class="card-text"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, quia.</p>
+                        <a href="" class="btn btn-primary"> Lorem, ipsum.
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card mx-2" style="width: 15rem;" style="height: 10rem;">
+                    <img src="../Base/baseMedia/ladyOnRocket.gif" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Project  - Lorem, ipsum.</h5>
+                        <p class="card-text"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, quia.</p>
+                        <a href="" class="btn btn-primary"> Lorem, ipsum.
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card mx-2" style="width: 15rem;" style="height: 10rem;">
+                    <img src="../Base/baseMedia/ladyOnRocket.gif" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Project  - Lorem, ipsum.</h5>
+                        <p class="card-text"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, quia.</p>
+                        <a href="" class="btn btn-primary"> Lorem, ipsum.
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card mx-2" style="width: 15rem;" style="height: 10rem;">
+                    <img src="../Base/baseMedia/ladyOnRocket.gif" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Project  - Lorem, ipsum.</h5>
+                        <p class="card-text"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, quia.</p>
+                        <a href="" class="btn btn-primary"> Lorem, ipsum.
+                        </a>
+                    </div>
+                </div>
+
+                <div class="card mx-2" style="width: 15rem;" style="height: 10rem;">
+                    <img src="../Base/baseMedia/ladyOnRocket.gif" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Project  - Lorem, ipsum.</h5>
+                        <p class="card-text"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, quia.</p>
+                        <a href="" class="btn btn-primary"> Lorem, ipsum.
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+    <script src="baseScript.js"></script>
+</body>
+
+</html>
