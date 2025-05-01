3cc5c631d44bc7d0841c822e293bc927284d22c8
Andrew Ahn <andrew.ahn@hotmail.co.uk>
2025-05-01 08:58:22 +0100

    start project 45

--- Project45/project45.php
+++ Project45/project45.php
@@ -11,7 +11,7 @@
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <link rel="stylesheet" href="../Base/baseStyle.css">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./project45.css">
    
 
     <title>Project Starter</title>
@@ -36,7 +36,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
- <script src="./script.js"></script>
+ <script src="./project45.js"></script>
 
 </body>
 
