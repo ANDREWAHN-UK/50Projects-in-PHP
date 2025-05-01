dc57f97f85350789651416423fd5d4d937b12829
Andrew Ahn <andrew.ahn@hotmail.co.uk>
2025-05-01 14:19:25 +0100

    add 47thumb;update project47;make tea; play with Grace

--- Project47/project47.php
+++ Project47/project47.php
@@ -20,7 +20,21 @@
 <body>
     <?php include '../Base/header.php'; ?>
 
-
+        <div class="testimonial-container">
+            <div class="progress-bar"></div>
+            <div class="fas fa-quote-left fa-quote"></div>
+            <div class="fas fa-quote-right fa-quote"></div>
+            <p class="testimonial">          "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
+            </p>
+            <div class="user">
+                <img src="https://randomuser.me/api/portraits/women/8.jpg" alt="user" class="user-image">
+                <div class="user-details">
+                    <h4 class="username">Miyah Miles</h4>
+                    <p class="role">Marketing</p>
+                </div>
+            </div>
+           
+        </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
