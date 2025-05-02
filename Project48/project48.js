873ed88d764c17e1bded837420c3c64f83537ae1
Andrew Ahn <andrew.ahn@hotmail.co.uk>
2025-05-02 10:32:50 +0100

    do project 49; add image

--- Project48/project48.js
+++ Project48/project48.js
@@ -1,4 +1,20 @@
-// Use the below to test the js file has loaded
+const imageContainer = document.querySelector('.image-container')
 
-window.onload=function () {alert('your js file works')}
+const imageURL = 'https://picsum.photos/'
 
+const rows = 10
+
+for (let i = 0; i < rows *3; i++){
+    const image = document.createElement('img')
+    image.src = `${imageURL}${getRandomSize()}`
+    imageContainer.appendChild(image)
+}
+
+
+function getRandomSize(){
+    return `${getRandomNumber()}/${getRandomNumber()}`
+}
+
+function getRandomNumber(){
+    return Math.floor(Math.random() *10) + 300
+}
